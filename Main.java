d7a4b8b29b2ccddd5a4e8695b0987f45157469df
SelC <selc@mail.ru>
2015-07-29 09:55:55 +0300

    Commit

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Cat aBarsic = new Cat("Barsic", 1, 2, "male");
 		Cat aMurka = new Cat("Murka", 2, 3, "female");
-		
+		// Test of changes
 		aBarsic.voice();
 		aBarsic.compareCats(aMurka);
 		Cat alittleCat = aBarsic.newCat(aMurka);
